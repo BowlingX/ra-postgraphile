d9699e9b110c4432a93ae1c730c2ba8abdcb7a2e
Christiaan Westerbeek <chris@devotis.nl>
2020-01-21 22:46:56 +0100

    Update src/buildQuery.ts
    Co-Authored-By: Benjie Gillam <benjie@jemjie.com>

--- src/buildQuery.ts
+++ src/buildQuery.ts
@@ -38,7 +38,7 @@ let typeMap: any
 let queryMap: QueryMap
 
 const mapType = (idType: any, value: string | number) =>
-  idType.name === 'String' ? value : idType.name === 'UUID' ? value : parseInt(value as string, 10)
+  ['UUID', 'Uuid', 'String'].includes(idType.name) ? value : parseInt(value as string, 10)
 
 export const buildQuery = (introspectionResults: any, factory: Factory) => (
   raFetchType: string,
