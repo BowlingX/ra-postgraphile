917b267826af2ece7c784ab1fec2f13dd791d3ae
Herman J. Radtke III <herman@hermanradtke.com>
2020-03-15 10:48:14 -0700

    Support alternative filter operators
    - simplify scalar filtering support
    - create FilterSpec to allow users to define alternative filters
    - remove experimental filter operator functions
    - create filter related types
    
    Writing a typesafe interface for alternative filters is not straight
    forward. This is due to the fact that a filter's correctness depends on
    the state of the Postgres database. That necessitates checking to be
    done at runtime. An additional complication is that a filter operator
    may be valid for a wide range of types.
    
    Because of the above contraints, we enforce an interface that is either
    a scalar, array of scalars or a FilterSpec. The FilterSpec is what
    allows alternative operators. The FilterSpec interface ensures that the
    operator is valid, but does not otherwise make any attempt to convert or
    modify the value. Thus, it is on the user to make sure the operator they
    choose is valid for both the field they are filtering on and the value
    they are filtering with.

--- src/__tests__/filters.spec.ts
+++ src/__tests__/filters.spec.ts
@@ -1,38 +1,62 @@
 /* tslint:disable:no-expression-statement */
-import type { IntrospectionNamedTypeRef } from 'graphql'
+import type { FilterSpec } from '../types'
 import { mapFilterType } from '../filters'
 
 describe('filters', () => {
   it('should filter by String', () => {
-    expect(
-      mapFilterType({ name: 'String' } as IntrospectionNamedTypeRef, 'value', 'id')
-    ).toMatchSnapshot()
+    expect(mapFilterType({ kind: 'SCALAR', name: 'String' }, 'value', 'id')).toMatchSnapshot()
+  })
+  it('should filter using a FilterSpec', () => {
+    const spec: FilterSpec = {
+      operator: 'likeInsensitive',
+      value: 'ilike value',
+    }
+
+    expect(mapFilterType({ kind: 'SCALAR', name: 'String' }, spec, 'id')).toMatchSnapshot()
   })
   it('should filter by Int', () => {
-    expect(mapFilterType({ name: 'Int' } as IntrospectionNamedTypeRef, 5, 'id')).toMatchSnapshot()
+    expect(mapFilterType({ kind: 'SCALAR', name: 'Int' }, 5, 'id')).toMatchSnapshot()
   })
   it('should filter by BigInt', () => {
-    expect(
-      mapFilterType({ name: 'BigInt' } as IntrospectionNamedTypeRef, 5, 'id')
-    ).toMatchSnapshot()
+    expect(mapFilterType({ kind: 'SCALAR', name: 'Int' }, 5, 'id')).toMatchSnapshot()
   })
   it('should filter by UUID', () => {
     expect(
-      mapFilterType(
-        { name: 'UUID' } as IntrospectionNamedTypeRef,
-        '02d07429-c2a7-4494-aec9-e8bde9176e86',
-        'id'
-      )
+      mapFilterType({ kind: 'SCALAR', name: 'UUID' }, '02d07429-c2a7-4494-aec9-e8bde9176e86', 'id')
     ).toMatchSnapshot()
   })
   it('should filter by boolean', () => {
     expect(
-      mapFilterType({ name: 'Boolean' } as IntrospectionNamedTypeRef, true, 'booleanField')
+      mapFilterType({ kind: 'SCALAR', name: 'Boolean' }, true, 'booleanField')
     ).toMatchSnapshot()
   })
-  it('should throw on unsupported types', () => {
+  it('should filter with Full Text', () => {
+    expect(mapFilterType({ kind: 'SCALAR', name: 'FullText' }, 'test', 'id')).toMatchSnapshot()
+  })
+  it('should support custom filters via objects', () => {
+    const spec: FilterSpec = {
+      operator: 'contains',
+      value: ['a', 'b'],
+    }
+    expect(mapFilterType({ kind: 'SCALAR', name: 'String' }, spec, 'id')).toMatchSnapshot()
+  })
+  it('should support a value of undefined', () => {
+    expect(mapFilterType({ kind: 'SCALAR', name: 'String' }, undefined, 'id')).toMatchSnapshot()
+  })
+  it('should support custom filters with a value of undefined', () => {
+    const spec: FilterSpec = {
+      operator: 'contains',
+      value: undefined,
+    }
+    expect(mapFilterType({ kind: 'SCALAR', name: 'String' }, spec, 'id')).toMatchSnapshot()
+  })
+  it('should throw an error if custom filter is not of type FilterSpec', () => {
     expect(() =>
-      mapFilterType({ name: 'Unsupported' } as IntrospectionNamedTypeRef, 'foo', 'br')
+      mapFilterType({ kind: 'SCALAR', name: 'String' }, {}, 'id')
+    ).toThrowErrorMatchingSnapshot()
+
+    expect(() =>
+      mapFilterType({ kind: 'SCALAR', name: 'String' }, { operator: undefined, value: 'foo' }, 'id')
     ).toThrowErrorMatchingSnapshot()
   })
 })
