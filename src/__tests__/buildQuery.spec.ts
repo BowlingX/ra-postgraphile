86369ab490e4fb255c26331f01036103d7ef1605
David Heidrich <me@bowlingx.com>
2020-07-17 16:43:09 +0200

    fix(types): fixed types in tests

--- src/__tests__/buildQuery.spec.ts
+++ src/__tests__/buildQuery.spec.ts
@@ -1,11 +1,12 @@
+import type { IntrospectionNamedTypeRef } from 'graphql'
 import { mapType } from '../buildQuery'
 
 describe('buildQuery', () => {
   it('mapType should map types', () => {
-    expect(mapType({ name: 'UUID' }, '1')).toEqual('1')
-    expect(mapType({ name: 'String' }, '1')).toEqual('1')
-    expect(mapType({ name: 'Uuid' }, '1')).toEqual('1')
-    expect(mapType({ name: 'BigInt' }, '1')).toEqual(1)
-    expect(mapType({ name: 'Int' }, '1')).toEqual(1)
+    expect(mapType(({ name: 'UUID' } as any) as IntrospectionNamedTypeRef<any>, '1')).toEqual('1')
+    expect(mapType(({ name: 'String' } as any) as IntrospectionNamedTypeRef<any>, '1')).toEqual('1')
+    expect(mapType(({ name: 'Uuid' } as any) as IntrospectionNamedTypeRef<any>, '1')).toEqual('1')
+    expect(mapType(({ name: 'BigInt' } as any) as IntrospectionNamedTypeRef<any>, '1')).toEqual(1)
+    expect(mapType(({ name: 'Int' } as any) as IntrospectionNamedTypeRef<any>, '1')).toEqual(1)
   })
 })
